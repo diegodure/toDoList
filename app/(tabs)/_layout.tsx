3d82d1c0bad310034875645c3242febce1c179ba
diegodure <didure@fctunca.edu.py>
2024-10-03 20:35:20 -0400

    Se agrega el archivo index

--- app/(tabs)/_layout.tsx
+++ app/(tabs)/_layout.tsx
@@ -16,6 +16,15 @@ export default function TabLayout() {
           tabBarActiveTintColor: Colors[colorScheme ?? 'light'].tint,
           headerShown: false,
         }}>
+          <Tabs.Screen
+        name="index"
+        options={{
+          title: 'Home',
+          tabBarIcon: ({ color, focused }) => (
+            <TabBarIcon name={focused ? 'home' : 'home-outline'} color={color} />
+          ),
+        }}
+      />
         <Tabs.Screen
         name="TaskList"
         options={{
